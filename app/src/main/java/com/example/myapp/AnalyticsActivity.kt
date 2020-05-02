c03ff71ea8224c39315453d8aae6d98e738ff25b
ashutosh <srivastava_ashutosh@lilly.com>
2020-05-03 03:08:56 +0530

    Added second screen to analyse

--- app/src/main/java/com/example/myapp/AnalyticsActivity.kt
+++ app/src/main/java/com/example/myapp/AnalyticsActivity.kt
@@ -1,8 +1,11 @@
 package com.example.myapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.myapp.provider.FirebaseProvider
+import kotlinx.android.synthetic.main.activity_analytics.*
 
 class AnalyticsActivity : AppCompatActivity() {
 
@@ -10,9 +13,14 @@ class AnalyticsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_analytics)
 
         analytics.register(FirebaseProvider(this))
         //analytics.register(LocalyticsProvider(this, ))
+
+        btn1.setOnClickListener(View.OnClickListener {
+            val intent = Intent(applicationContext, SecondActivity::class.java)
+            startActivity(intent)
+        })
     }
 }
